668a854f17e4179c77f00a875d569177d286cda4
yasin <y.mesekoparan1510@gmail.com>
2022-10-17 23:48:54 +0300

    17.10

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -4,9 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.opera.OperaDriver;
+
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
